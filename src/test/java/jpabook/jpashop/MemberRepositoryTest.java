16f666051bc10e2787525979ceaa11b2171a16c7
kickoutter <kickoutter.code@gmail.com>
2022-05-02 18:30:03 +0900

    1.프로젝트 환경설정 - 수정

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,15 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MemoryRepositoryTest {
+public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
