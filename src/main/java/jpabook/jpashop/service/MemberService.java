a2dfb949b51abd0fa944d9bd44200ccad460cf47
kickoutter <kickoutter.code@gmail.com>
2022-05-03 20:19:44 +0900

    4.회원 도메인 개발 - 2)회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // lombok, final붙은 필드(멤버변수, 속성)들만 인젝션 해줌
+public class MemberService {
+
+    /**
+     * 생성자 주입
+     */
+//    private final MemberRepository memberRepository;
+//
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * lombok 적용한 생성자 주입, 맨 위 @ReauiredArgsConstructor 어노테이션
+     */
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입 (쓰기)
+     */
+    @Transactional(readOnly = false) // 변경
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회 (읽기)
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
